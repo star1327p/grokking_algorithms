9306432a1b51dce356bf90b1e3db03749da13f0f
Adnan <adnan9011@gmail.com>
2023-08-09 02:41:22 +0330

    Update 05_quicksort.kt (#267)
    remove unnecessary line

--- 04_quicksort/kotlin/05_quicksort.kt
+++ 04_quicksort/kotlin/05_quicksort.kt
@@ -3,10 +3,9 @@ fun quickSort(list: List<Int>): List<Int> {
     if (list.size <= 1) return list
     // recursive case
     val pivot = list[list.size / 2]
-    val equal = list.filter { it == pivot }
     val less = list.filter { it < pivot }
     val greater = list.filter { it > pivot }
-    return quickSort(less) + equal + quickSort(greater)
+    return quickSort(less) + pivot + quickSort(greater)
 }
 
 fun main(args: Array<String>) = println(quickSort(listOf(10, 5, 2, 3))) // [2, 3, 5, 10]
