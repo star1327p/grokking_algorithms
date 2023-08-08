8969269a10e2ed194b9b8b163ff91f2a75bee6c6
Adnan <adnan9011@gmail.com>
2023-08-09 02:40:53 +0330

    Update 04_recursive_max.kt (#266)
    add an argument to when block

--- 04_quicksort/kotlin/04_recursive_max.kt
+++ 04_quicksort/kotlin/04_recursive_max.kt
@@ -1,5 +1,5 @@
-private fun max(list: IntArray): Int = when {
-    list.size == 2 -> if (list[0] > list[1]) list[0] else list[1]
+private fun max(list: IntArray): Int = when (list.size) {
+    2 -> if (list[0] > list[1]) list[0] else list[1]
     else -> {
         val subMax = max(list.copyOfRange(1, list.size))
         if (list[0] > subMax) list[0] else subMax
