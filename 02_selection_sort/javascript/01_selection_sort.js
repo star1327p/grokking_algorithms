f7ccf02e001c06f39a65684bfd3540c0f8476ba7
ViMaCode <59506333+Vitalii-Maevskii@users.noreply.github.com>
2024-03-22 23:10:02 +0300

    Array variable (#259)
    The array is set in one place.

--- 02_selection_sort/javascript/01_selection_sort.js
+++ 02_selection_sort/javascript/01_selection_sort.js
@@ -40,7 +40,7 @@ function selectionSort(array) {
 }
 
 const sourceArray = [5, 3, 6, 2, 10];
-const sourtedArray = selectionSort([5, 3, 6, 2, 10]);
+const sourtedArray = selectionSort(sourceArray);
 
 console.log("Source array - ", sourceArray); // [5, 3, 6, 2, 10]
 console.log("New sorted array - ", sourtedArray); // [2, 3, 5, 6, 10]
